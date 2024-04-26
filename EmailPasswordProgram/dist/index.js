335c38801d0a8a39dc1bf02fa64d814d8e146be8
onnaib <onnaibtariq961@gmail.com>
2024-04-26 22:22:39 +0500

    ATM Project

---
+++ EmailPasswordProgram/dist/index.js
@@ -0,0 +1,22 @@
+import inquirer from "inquirer";
+let email = "xyz7@gmail.com";
+let password = "rockstar";
+const data = await inquirer.prompt([
+    {
+        type: 'input',
+        name: 'email',
+        massage: 'Email:   '
+    },
+    {
+        type: 'password',
+        name: 'password',
+        message: 'Password:  '
+    },
+]);
+if (data.email === "xyz7@gmail.com" && data.password === "rockstar") {
+    console.log("Email:   " + data.email + "\nPassword: " + data.password);
+    console.log("LOGGED IN");
+}
+else if (data.email === "xyz7@gmail.com" || data.password === "rockstar") {
+    console.log("Invailed Email or Password");
+}
